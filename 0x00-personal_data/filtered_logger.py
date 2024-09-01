9328b781bcc8bcab2bff270242a8d6c11e47b365
Favour04 <idaeworfavour1@gmail.com>
2024-09-01 14:35:08 +0100

    logs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import re
 def filter_datum(fields, redaction, message, separator):
-    for field in fields:
-        message = re.sub(f'{field}=[^{separator}]*', f'{field}={redaction}', message)
+    for field in fields: message = re.sub(f'{field}=[^{separator}]*', f'{field}={redaction}', message)
     return message
