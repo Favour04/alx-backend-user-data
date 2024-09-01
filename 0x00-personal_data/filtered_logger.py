af5b81743c41fcf3672aba075894acb849f03e30
Favour04 <idaeworfavour1@gmail.com>
2024-09-01 14:24:52 +0100

    log

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
-# import logging
 import re
+"""
+    logging module
+"""
 
-# logger = logging.getLogger('__name__')
-# logger.setLevel(logging.INFO)
-
-# stream_handler = logging.StreamHandler()
-# formater = logging.Formatter('%(message)s')
-
-# stream_handler.setFormatter(formater)
-
-# logger.addHandler(stream_handler)
 
 def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+    """
+        returns the log message obfuscated
+    """
     for field in fields:
         pattern = f'{field}=[^{separator}]*'
         message = re.sub(pattern, f'{field}={redaction}', message)
