eaf8ec635927d3b849c8c9842ac465ee278a68a2
Favour04 <idaeworfavour1@gmail.com>
2024-09-01 17:11:59 +0100

    logged

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-import re
-from typing import List
 """This module contains a function that returns the log message obfuscated
 """
+import re
+from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """This function returns the log message obfuscated
+    """
     for field in fields:
         pattern = f'{field}=[^{separator}]*'
         message = re.sub(pattern, f'{field}={redaction}', message)
