debe10dcf949fca887099c0079a7082672144f08
Favour04 <idaeworfavour1@gmail.com>
2024-09-04 21:47:19 +0100

    done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 """
 from .auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -40,3 +42,42 @@ class BasicAuth(Auth):
                                     'utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """Extract user credentials
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in list(decoded_base64_authorization_header):
+            return None, None
+        else:
+            credentails = decoded_base64_authorization_header.split(':')
+            return credentails[0], credentails[1]
+
+    def user_object_from_credentials(self, user_email:
+                                     str, user_pwd: str) -> TypeVar('User'):
+        """Create user object from credentials
+        """
+        if not isinstance(user_email, str):
+            return None
+        
+        if not isinstance(user_pwd, str):
+            return None
+
+        User.load_from_file()
+        if User.count() == 0:
+            return None
+        elif User.search({'email': user_email}) is not None:
+            for user in User.search({'email': user_email}):
+                if user.is_valid_password(user_pwd):
+                    return user
+                else:
+                    return None
+        else:
+            return None
+            
