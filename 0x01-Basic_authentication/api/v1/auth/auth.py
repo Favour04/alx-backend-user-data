537ac16ce96e5b7c0b3a5258a20298f6290f3473
Favour04 <idaeworfavour1@gmail.com>
2024-09-04 10:44:12 +0100

    task 5 done

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,10 +8,30 @@ from typing import List, TypeVar
 class Auth:
     """Auth class
     """
+    def tog_str(self, string: str) -> str:
+        """This method add / or remove slash
+           from a string for / resilient comparison
+        """
+        if string[-1] != '/':
+            string = f'{string}/'
+        else:
+            string = string[:-1]
+
+        return string
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require authentication
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        if path in excluded_paths or self.tog_str(path) in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """Authorization header
