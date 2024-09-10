5fbe704d057b6d4a6f6c8d0eb4372639ea618bd5
Favour04 <idaeworfavour1@gmail.com>
2024-09-10 23:14:35 +0100

    user created

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,51 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def new(self, obj) -> None:
+        """Add obj to the current session
+        """
+        self._session.add(obj)
+
+    def save(self) -> None:
+        """Save obj to the database
+        """
+        self._session.commit()
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a user to the database
+        """
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self.new(user)
+            self.save()
+            return user
+        except Exception as e:
+            self._session.rollback()
+            raise e
