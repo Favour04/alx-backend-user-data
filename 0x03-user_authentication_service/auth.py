beede60db41d2e8ebaaeb3d3368136c6d614b757
Favour04 <idaeworfavour1@gmail.com>
2024-09-11 00:56:21 +0100

    added hashing function in auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,9 @@
 """Authentication module
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -9,3 +12,24 @@ def _hash_password(password: str) -> bytes:
     """
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """This method is used to create new user
+        """
+        try:
+            if self._db.find_user_by(email=email):
+                raise ValueError(f'User {email} already exists.')
+        except NoResultFound:
+            if not isinstance(password, bytes):
+                password = _hash_password(password)
+
+            user = self._db.add_user(email, password)
+            return user
