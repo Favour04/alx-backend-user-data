c50b38b8e419e9ee6d2c4cd94fbe1a84d65a9f81
Favour04 <idaeworfavour1@gmail.com>
2024-09-11 02:30:54 +0100

    added user post route to flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -11,6 +13,28 @@ def get_index():
     """Get index
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def user():
+    if request.method == "POST":
+        if request.is_json:
+            data = request.get_json
+        else:
+            data = request.form
+
+        email = data.get('email')
+        password = data.get('password')
+
+        if not email or not password:
+            return jsonify({"message": "Missing email or password"}), 400
+
+        try:
+            user = AUTH.register_user(email, password)
+            return jsonify({"email": user.email, "message":
+                            "user created"}), 201
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
