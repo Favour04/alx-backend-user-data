8989bcc2303c9c16efa9fc2a47eb7066b8726943
Favour04 <idaeworfavour1@gmail.com>
2024-09-11 02:39:29 +0100

    added user post route to flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -34,7 +34,7 @@ def user():
         try:
             user = AUTH.register_user(email, password)
             return jsonify({"email": user.email, "message":
-                            "user created"}), 201
+                            "user created"})
         except ValueError:
             return jsonify({"message": "email already registered"}), 400
 
